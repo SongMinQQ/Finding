da5be69d66b83d9a1d3a569f838f44bef7f9b54f
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-10-16 00:10:52 +0900

    -

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <SafeAreaProvider>
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
           <StatusBar style="auto" />
         </View>
     </SafeAreaProvider>
