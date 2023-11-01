9debe289d1d487b11cf581e3bbcd80e59a693c42
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-11-01 21:53:43 +0900

    지역선택 UI

--- src/police/PoliceFind.js
+++ src/police/PoliceFind.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import CountySelect from './CountySelect';
+import CitySelect from './CitySelect';
 
 const PoliceFind = () => {
+    const [selectedCity, setSelectedCity] = useState('서울');
+    const [selectedCounty, setSelectedCounty] = useState('강남');
+    const [searchText, setSearchText] = useState(''); // 검색창의 상태
+
+    const cityChange = (city) => {
+        setSelectedCity(city);
+    }
+
+    const countyChange = (county) => {
+        setSelectedCounty(county);
+    }
+
+    useEffect(() => {
+        console.log(selectedCity);
+        console.log(selectedCounty);
+    }, [selectedCounty]);
+
     return (
-        <View>
+        <View style={styles.container}>
             <Text>경찰청api 페이지</Text>
+            <View style={styles.row}>
+                <CitySelect changeFunction={cityChange} city={selectedCity} />
+                <CountySelect changeFunction={countyChange} county={selectedCounty} city={selectedCity} />
+                <TextInput 
+                    style={styles.searchInput}
+                    value={searchText}
+                    onChangeText={setSearchText}
+                    placeholder="검색..."
+                />
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    searchInput: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        paddingLeft: 10,
+        marginLeft: 10
+    }
+});
 
 export default PoliceFind;
