e4bef20b5d9ad55e93fc0df40072dd0c6e3f94f5
minseokKang <min1042004444@gmail.com>
2023-11-05 15:46:47 +0900

    프로필 게시글 목록 배치 수정

--- src/profile/ProfileFind.js
+++ src/profile/ProfileFind.js
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 const WINDOW_WIDTH = Dimensions.get('window').width;
 
 const ITEM_SIZE = WINDOW_WIDTH * 0.3;
+const ITEM_PADDING_SIZE = WINDOW_WIDTH * 0.1 / 4;
 const ITEM_BORDER_RADIUS = ITEM_SIZE * 0.08;
 const ITEM_TEXT_SIZE_LARGE = ITEM_SIZE * 0.15;
 const ITEM_TEXT_SIZE_SMALL = ITEM_SIZE * 0.12;
@@ -12,35 +13,39 @@ const ITEM_TEXT_SIZE_SMALL = ITEM_SIZE * 0.12;
 const ProfileFind = () => {
     const navigation = useNavigation();
 
-
-    const findItemData = [...Array(21)].map((_, index) => ({
+    const findItemData = [...Array(20)].map((_, index) => ({
         id: index,
         imgURL: `https://picsum.photos/id/${index}/200/200`,
         itemName: `물건 ${index + 1}`,
         category: `전자기기`,
         location: `위치 ${index + 1}`,
-        date: `2023-10-${index+1}` 
+        date: `2023-10-${index + 1}`
     }));
 
     return (
         <ScrollView>
             <View style={styles.container}>
                 {findItemData.map((item) => (
-                    <TouchableOpacity key={item.id} onPress={() => navigation.navigate("FindBoardDetail", {
-                        imgURL: item.imgURL,
-                        itemName: item.itemName,
-                        category: item.category,
-                        location: item.location,
-                        date: item.date,
-                    })}>
-                        <View key={item.id} style={styles.item}>
-                            <Image
-                                source={{uri:item.imgURL}}
-                                style={styles.itemImage}
-                            />
-                            <Text style={styles.itemName}>{item.itemName}</Text>
-                            <Text style={styles.itemLocation}>{item.location}</Text>
-                        </View>
+                    <TouchableOpacity
+                        key={item.id}
+                        style={[
+                            styles.item,
+                            { marginRight: (item.id % 3) !== 2 ? 0 : ITEM_PADDING_SIZE } // 세 번째 아이템에는 마진을 적용하지 않음
+                        ]}
+                        onPress={() => navigation.navigate("FindBoardDetail", {
+                            imgURL: item.imgURL,
+                            itemName: item.itemName,
+                            category: item.category,
+                            location: item.location,
+                            date: item.date,
+                        })}>
+
+                        <Image
+                            source={{ uri: item.imgURL }}
+                            style={styles.itemImage}
+                        />
+                        <Text style={styles.itemName}>{item.itemName}</Text>
+                        <Text style={styles.itemLocation}>{item.location}</Text>
                     </TouchableOpacity>
                 ))}
             </View>
@@ -52,7 +57,7 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         flexWrap: 'wrap',
-        justifyContent: 'space-evenly',
+        justifyContent: 'flex-start',
     },
     item: {
         width: ITEM_SIZE,
@@ -60,6 +65,7 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         alignItems: 'center',
         justifyContent: 'center',
+        marginLeft: ITEM_PADDING_SIZE,
     },
     itemImage: {
         width: ITEM_SIZE,
--- src/profile/ProfileLost.js
+++ src/profile/ProfileLost.js
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 const WINDOW_WIDTH = Dimensions.get('window').width;
 
 const ITEM_SIZE = WINDOW_WIDTH * 0.3;
+const ITEM_PADDING_SIZE = WINDOW_WIDTH * 0.1 / 4;
 const ITEM_BORDER_RADIUS = ITEM_SIZE * 0.08;
 const ITEM_TEXT_SIZE_LARGE = ITEM_SIZE * 0.15;
 const ITEM_TEXT_SIZE_SMALL = ITEM_SIZE * 0.12;
@@ -12,33 +13,38 @@ const ITEM_TEXT_SIZE_SMALL = ITEM_SIZE * 0.12;
 const ProfileLost = () => {
     const navigation = useNavigation();
 
-    const lostItemData = [...Array(21)].map((_, index) => ({
+    const lostItemData = [...Array(20)].map((_, index) => ({
         id: index,
-        imgURL: `https://picsum.photos/id/${index+21}/200/200`,
+        imgURL: `https://picsum.photos/id/${index + 21}/200/200`,
         itemName: `물건 ${index + 22}`,
         category: `전자기기`,
         location: `위치 ${index + 22}`,
-        date: `2023-10-${index+1}` 
+        date: `2023-10-${index + 1}`
     }));
     return (
         <ScrollView>
             <View style={styles.container}>
                 {lostItemData.map((item) => (
-                    <TouchableOpacity key={item.id} onPress={() => navigation.navigate("LostBoardDetail", {
-                        imgURL: item.imgURL,
-                        itemName: item.itemName,
-                        category: item.category,
-                        location: item.location,
-                        date: item.date,
-                    })}>
-                        <View key={item.id} style={styles.item}>
-                            <Image
-                                source={{uri:item.imgURL}}
-                                style={styles.itemImage}
-                            />
-                            <Text style={styles.itemName}>{item.itemName}</Text>
-                            <Text style={styles.itemLocation}>{item.location}</Text>
-                        </View>
+                    <TouchableOpacity
+                        key={item.id}
+                        style={[
+                            styles.item,
+                            { marginRight: (item.id % 3) !== 2 ? 0 : ITEM_PADDING_SIZE } // 세 번째 아이템에는 마진을 적용하지 않음
+                        ]}
+                        onPress={() => navigation.navigate("FindBoardDetail", {
+                            imgURL: item.imgURL,
+                            itemName: item.itemName,
+                            category: item.category,
+                            location: item.location,
+                            date: item.date,
+                        })}>
+                        <Image
+                            source={{ uri: item.imgURL }}
+                            style={styles.itemImage}
+                        />
+                        <Text style={styles.itemName}>{item.itemName}</Text>
+                        <Text style={styles.itemLocation}>{item.location}</Text>
+
                     </TouchableOpacity>
                 ))}
             </View>
@@ -50,7 +56,7 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         flexWrap: 'wrap',
-        justifyContent: 'space-evenly',
+        justifyContent: 'flex-start',
     },
     item: {
         width: ITEM_SIZE,
@@ -58,6 +64,7 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         alignItems: 'center',
         justifyContent: 'center',
+        marginLeft: ITEM_PADDING_SIZE,
     },
     itemImage: {
         width: ITEM_SIZE,
