37bf7c302ffab69bfdb49eaf0bf9ae9a9481714d
minseokKang <min1042004444@gmail.com>
2023-11-17 18:43:49 +0900

    아이디 비번 찾기 디자인 수정 + 안드로이드 결제 문제 해결

--- src/Login/FindIdPhoneNumber.js
+++ src/Login/FindIdPhoneNumber.js
@@ -22,31 +22,25 @@ const FindIdPhoneNumber = ({ navigation }) => {
     },
   };
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+    
       <View style={styles.flexbox}>
-        <Image style={styles.image} source={require('../../img/loginIcon.png')} />
-        {/* 임시 로고(이미지로 만들 경우 교체해야 함) */}
-        <Text style={styles.logoText}>Finding</Text>
-        <Text style={[styles.logoText, { fontSize: WINDOW_HEIGHT * 0.017, marginBottom: WINDOW_HEIGHT * 0.1 }]}>핸드폰 번호를 입력하시오.</Text>
-
         <TextInput style={styles.inputView}
           mode="outlined"
           placeholder='핸드폰번호'
           onChangeText={_handleUserPhonNumberChange}
+          keyboardType={'phone-pad'}
           theme={theme}
         />
 
-        <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.correctBtn}
             onPress={() => {
               navigation.navigate("Home")
               console.log(`userPhonNumber: ${userPhonNumber}`);
             }}
-          ><Text style={{ fontWeight: 'bold', color: 'white' }}>인증번호 받으러 가기</Text>
+          ><Text style={{ fontWeight: 'bold', color: 'white' }}>인증번호 받기</Text>
           </TouchableOpacity>
-        </View>
+
       </View>
-    </SafeAreaView>
   );
 };
 
@@ -56,55 +50,26 @@ export default FindIdPhoneNumber;
 const styles = StyleSheet.create({
 
   flexbox: {
+    flex: 1,
+    width: '100%',
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "flex-start",
-    paddingVertical: WINDOW_HEIGHT * 0.07,
+    paddingVertical: WINDOW_HEIGHT * 0.04,
   },
 
-  container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  image: {
-    width: WINDOW_HEIGHT * 0.1,
-    height: WINDOW_HEIGHT * 0.1,
-    margin: WINDOW_HEIGHT * 0.02,
-  },
-
-  logoText: {
-    fontWeight: 'bold',
-    fontSize: WINDOW_HEIGHT * 0.06,
-  },
-  buttonContainer: {
-    width: '60%',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
   inputView: {
     backgroundColor: "#fff",
     width: "90%",
-    marginBottom: WINDOW_HEIGHT * 0.02,
   },
   correctBtn: {
-    width: "100%",
+    width: "90%",
     borderRadius: 25,
     height: WINDOW_HEIGHT * 0.06,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: WINDOW_HEIGHT * 0.01,
+    marginTop: WINDOW_HEIGHT * 0.02,
     backgroundColor: "#007bff",
   },
-  forgotBox: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  forgot_button: {
-    margin: WINDOW_HEIGHT * 0.02,
-  },
-
-
 
 });
--- src/Login/FindPassword.js
+++ src/Login/FindPassword.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { View, Text, Button, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { MD3LightTheme as DefaultTheme, } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Image } from 'expo-image';
+import styled from 'styled-components';
+import FindPasswordEmail from './FindPasswordEmail';
+import FindPasswordPhoneNumber from './FindPasswordPhoneNumber';
 
 const WINDOW_HEIGHT = Dimensions.get('window').height;
+
+const FindIdBox = styled.TouchableOpacity`
+  flex: 1;
+  align-itmes: center;
+  justify-content: center;
+  background-color: ${props => props.findPW};
+  border-color: #EEEEEE;
+  border-top-width: 1px;
+  border-left-width: 1px;
+  border-right-width: ${props => props.isFirst ? '0px' : '1px'};
+`;
+const RowSelectText = styled.Text`
+  text-align: center;
+  color: ${props => props.findPW};
+  font-weight: bold;
+`;
 
 const FindPassword = ({ navigation }) => {
   // TextInput 클릭 시 테두리 색 변경하는 코드
@@ -17,23 +36,31 @@ const FindPassword = ({ navigation }) => {
       primary: '#007bff', // 이거 바꾸면 됨
     },
   }
-  
-    return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={styles.flexbox}>
-        <Image style={styles.image} source={require('../../img/loginIcon.png')} />
-        {/* 임시 로고(이미지로 만들 경우 교체해야 함) */}
-        <Text style={styles.logoText}>Finding</Text>
-        <Text style={[styles.logoText, { fontSize: WINDOW_HEIGHT * 0.017, marginBottom: WINDOW_HEIGHT * 0.1 }]}>비밀번호 찾기 페이지!</Text>
-        
-          <View style ={styles.forgotBox}>
-          <TouchableOpacity style = {styles.forgot_button} onPress={() => navigation.navigate("Find Password Email")}><Text>비밀번호 이메일로 찾기</Text></TouchableOpacity>
-          <TouchableOpacity style = {styles.forgot_button} onPress={() => navigation.navigate("Find Password PhoneNumber")}><Text>비밀번호 핸드폰으로 찾기</Text></TouchableOpacity>
-          </View>
-          </View> 
-        </SafeAreaView>
-        
-    );
+  const [findPW, setFindPW] = useState('Email');
+
+  return (
+    <View style={styles.flexbox}>
+      <View style={styles.rowSelectBox}>
+        <FindIdBox findPW={findPW === "Email" ? "#fff" : "#EEEEEE"} isFirst={true}
+          onPress={() => {
+            //navigation.navigate("Find Id Email")
+            setFindPW('Email');
+          }}>
+          <RowSelectText findPW={findPW === "Email" ? "#000" : "#DADADA"}>이메일로 찾기</RowSelectText>
+        </FindIdBox>
+        <FindIdBox findPW={findPW === "Email" ? "#EEEEEE" : "#fff"}
+          onPress={() => {
+            //navigation.navigate("Find Id PhoneNumber")
+            setFindPW('PhoneNum');
+          }}>
+          <RowSelectText findPW={findPW === "Email" ? "#DADADA" : "#000"}>핸드폰으로 찾기</RowSelectText>
+        </FindIdBox>
+      </View>
+        {findPW === 'Email' ? <FindPasswordEmail navigation={navigation} /> : <FindPasswordPhoneNumber  navigation={navigation}/>}
+
+    </View>
+
+  );
 };
 
 export default FindPassword;
@@ -41,54 +68,19 @@ export default FindPassword;
 
 const styles = StyleSheet.create({
   flexbox: {
+    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "flex-start",
-    paddingVertical: WINDOW_HEIGHT * 0.07,
+    paddingVertical: WINDOW_HEIGHT * 0.04,
+  },
+  rowSelectBox: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    width: '90%',
+    height: WINDOW_HEIGHT * 0.07,
   },
 
-  container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
 
-  image: {
-    width: WINDOW_HEIGHT * 0.1,
-    height: WINDOW_HEIGHT * 0.1,
-    margin: WINDOW_HEIGHT * 0.02,
-  },
-
- logoText: {
-    fontWeight: 'bold',
-    fontSize: WINDOW_HEIGHT * 0.06,
-  },
-  buttonContainer: {
-    width: '60%',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  inputView: {
-    backgroundColor: "#fff",
-    width: "90%",
-    marginBottom: WINDOW_HEIGHT * 0.02,
-  },
-  correctBtnBtn: {
-    width: "100%",
-    borderRadius: 25,
-    height: WINDOW_HEIGHT * 0.06,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: WINDOW_HEIGHT * 0.01,
-    backgroundColor: "#007bff",
-  },
-  forgotBox: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  forgot_button: {
-    margin: WINDOW_HEIGHT * 0.02,
-  },
- 
 
 });
