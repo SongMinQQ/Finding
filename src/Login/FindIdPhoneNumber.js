6d6c325770802c05ff3038c933b615189ed708b8
minseokKang <min1042004444@gmail.com>
2023-11-17 15:48:35 +0900

    아이디 및 비밀번호 찾기 오류 수정

--- src/Login/FindIdPhoneNumber.js
+++ src/Login/FindIdPhoneNumber.js
@@ -1,39 +1,53 @@
-import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image } from 'expo-image';
+import { TextInput } from 'react-native-paper';
+import { MD3LightTheme as DefaultTheme, } from 'react-native-paper';
 
-const FindIdPhoneNumber = ({navigation}) => {
+const WINDOW_HEIGHT = Dimensions.get('window').height;
 
-  const [userPhonNumber, setUserPhonNumber] = useState(''); 
+const FindIdPhoneNumber = ({ navigation }) => {
+
+  const [userPhonNumber, setUserPhonNumber] = useState('');
   const _handleUserPhonNumberChange = text => {
     setUserPhonNumber(text);
   }
-    return(
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+  const theme = {
+    ...DefaultTheme,
+    myOwnProperty: true,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#007bff', // 이거 바꾸면 됨
+    },
+  };
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
       <View style={styles.flexbox}>
-      <Image style={styles.image} source={require('../../img/loginIcon.png')} />
-      {/* 임시 로고(이미지로 만들 경우 교체해야 함) */}
-      <Text style={styles.logoText}>Finding</Text>
-      <Text style={[styles.logoText, { fontSize: WINDOW_HEIGHT * 0.017, marginBottom: WINDOW_HEIGHT * 0.1 }]}>핸드폰 번호를 입력하시오.</Text>
-      
-      <TextInput style={styles.inputView}
-        mode="outlined"
-        placeholder='핸드폰번호'
-        onChangeText={_handleUserPhonNumberChange}
-        theme={theme}
-      />
+        <Image style={styles.image} source={require('../../img/loginIcon.png')} />
+        {/* 임시 로고(이미지로 만들 경우 교체해야 함) */}
+        <Text style={styles.logoText}>Finding</Text>
+        <Text style={[styles.logoText, { fontSize: WINDOW_HEIGHT * 0.017, marginBottom: WINDOW_HEIGHT * 0.1 }]}>핸드폰 번호를 입력하시오.</Text>
 
-          <View style={styles.buttonContainer}>
+        <TextInput style={styles.inputView}
+          mode="outlined"
+          placeholder='핸드폰번호'
+          onChangeText={_handleUserPhonNumberChange}
+          theme={theme}
+        />
+
+        <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.correctBtn}
-          onPress={() => {
-            navigation.navigate("Home")
-            console.log(`userPhonNumber: ${userPhonNumber}`);
-          }}
-        ><Text style={{ fontWeight: 'bold', color: 'white' }}>인증번호 받으러 가기</Text>
-        </TouchableOpacity>
-        </View> 
-        </View> 
-      </SafeAreaView>
-    );
+            onPress={() => {
+              navigation.navigate("Home")
+              console.log(`userPhonNumber: ${userPhonNumber}`);
+            }}
+          ><Text style={{ fontWeight: 'bold', color: 'white' }}>인증번호 받으러 가기</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 
@@ -60,7 +74,7 @@ const styles = StyleSheet.create({
     margin: WINDOW_HEIGHT * 0.02,
   },
 
- logoText: {
+  logoText: {
     fontWeight: 'bold',
     fontSize: WINDOW_HEIGHT * 0.06,
   },
@@ -90,7 +104,7 @@ const styles = StyleSheet.create({
   forgot_button: {
     margin: WINDOW_HEIGHT * 0.02,
   },
- 
+
 
 
 });
--- src/Login/FindPasswordEmail.js
+++ src/Login/FindPasswordEmail.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { View, Text, TextInput, Button , StyleSheet} from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { MD3LightTheme as DefaultTheme, } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image } from 'expo-image';
 const WINDOW_HEIGHT = Dimensions.get('window').height;
 
-const FindIdEmail = ({ navigation }) => {
+const FindPasswordEmail = ({ navigation }) => {
   // TextInput 클릭 시 테두리 색 변경하는 코드
   const theme = {
     ...DefaultTheme,
@@ -13,23 +16,23 @@ const FindIdEmail = ({ navigation }) => {
       primary: '#007bff', // 이거 바꾸면 됨
     },
   }
-   // 유저 이메일 state
-   const [userID, setUserID] = useState('');
-   const [userEmail, setUserEmail] = useState(''); 
-   const _handleUserIDChange = text => {
+  // 유저 이메일 state
+  const [userID, setUserID] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const _handleUserIDChange = text => {
     setUserID(text);
   }
-   const _handleUserEmailChange = text => {
-     setUserEmail(text);
-   }
-    return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={styles.flexbox}>
+  const _handleUserEmailChange = text => {
+    setUserEmail(text);
+  }
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={styles.flexbox}>
         <Image style={styles.image} source={require('../../img/loginIcon.png')} />
         {/* 임시 로고(이미지로 만들 경우 교체해야 함) */}
         <Text style={styles.logoText}>Finding</Text>
         <Text style={[styles.logoText, { fontSize: WINDOW_HEIGHT * 0.017, marginBottom: WINDOW_HEIGHT * 0.1 }]}>이메일을 입력하시오.</Text>
-        
+
         <TextInput style={styles.inputView}
           mode="outlined"
           placeholder='이메일'
@@ -37,19 +40,19 @@ const FindIdEmail = ({ navigation }) => {
           theme={theme}
         />
 
-            <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.correctBtn}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.correctBtn}
             onPress={() => {
               navigation.navigate("Home")
               console.log(`userEmail: ${userEmail}`);
             }}
           ><Text style={{ fontWeight: 'bold', color: 'white' }}>인증번호 받으러 가기</Text>
           </TouchableOpacity>
-          </View> 
-          </View> 
-        </SafeAreaView>
-        
-    );
+        </View>
+      </View>
+    </SafeAreaView>
+
+  );
 };
 
 
@@ -57,56 +60,55 @@ export default FindPasswordEmail;
 
 const styles = StyleSheet.create({
 
-    flexbox: {
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        paddingVertical: WINDOW_HEIGHT * 0.07,
-      },
-    
-      container: {
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-    
-      image: {
-        width: WINDOW_HEIGHT * 0.1,
-        height: WINDOW_HEIGHT * 0.1,
-        margin: WINDOW_HEIGHT * 0.02,
-      },
-    
-     logoText: {
-        fontWeight: 'bold',
-        fontSize: WINDOW_HEIGHT * 0.06,
-      },
-      buttonContainer: {
-        width: '60%',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-      },
-      inputView: {
-        backgroundColor: "#fff",
-        width: "90%",
-        marginBottom: WINDOW_HEIGHT * 0.02,
-      },
-    
-      correctBtnBtn: {
-        width: "100%",
-        borderRadius: 25,
-        height: WINDOW_HEIGHT * 0.06,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: WINDOW_HEIGHT * 0.01,
-        backgroundColor: "#007bff",
-      },
+  flexbox: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingVertical: WINDOW_HEIGHT * 0.07,
+  },
 
-      forgotBox: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-      },
-      forgot_button: {
-        margin: WINDOW_HEIGHT * 0.02,
-      },
-     
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: WINDOW_HEIGHT * 0.1,
+    height: WINDOW_HEIGHT * 0.1,
+    margin: WINDOW_HEIGHT * 0.02,
+  },
+
+  logoText: {
+    fontWeight: 'bold',
+    fontSize: WINDOW_HEIGHT * 0.06,
+  },
+  buttonContainer: {
+    width: '60%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  inputView: {
+    backgroundColor: "#fff",
+    width: "90%",
+    marginBottom: WINDOW_HEIGHT * 0.02,
+  },
+
+  correctBtn: {
+    width: "100%",
+    borderRadius: 25,
+    height: WINDOW_HEIGHT * 0.06,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#007bff",
+  },
+
+  forgotBox: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  forgot_button: {
+    margin: WINDOW_HEIGHT * 0.02,
+  },
+
 });
