4cf771fe7814538f04c3a3baf69f7b6465f32a20
minseokKang <min1042004444@gmail.com>
2023-11-21 17:28:43 +0900

    로그인 기능 구현(로그인만 됨)

--- src/chatting/Chatting.js
+++ src/chatting/Chatting.js
@@ -1,34 +1,50 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Feather, MaterialIcons } from '@expo/vector-icons';
 import { FlatList } from 'react-native-gesture-handler';
 
 const Chatting = () => {
-    return (
-        <View style={styles.main}>
-            <View style={styles.textArea}>
-                <FlatList inverted={true}>
+    const [messages, setMessages] = useState([])
 
-                </FlatList>
-                <TextInput style={styles.input}/>
-                <TouchableOpacity>
-                    <MaterialIcons name="add-a-photo" size={24} color="black" />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Feather name="send" size={24} color="black" style={styles.sendButton}/>
-                </TouchableOpacity>
-            </View>
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://picsum.photos/id/1/200/200',
+                },
+            },
+        ])
+    }, [])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages =>
+            GiftedChat.append(previousMessages, messages),
+        )
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    main: {
+    container: {
         flex: 1,
-        backgroundColor:"#ffffff",
-        justifyContent: "flex-end",
-        // alignItems: 'center',
-        padding: 16,
+        backgroundColor: "#ffffff",
     },
     input: {
         flex: 1,
