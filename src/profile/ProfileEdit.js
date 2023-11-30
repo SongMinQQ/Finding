b27929f0777ed6420f03e278130622afc9fb074a
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-11-30 23:02:21 +0900

    프로필 수정 기능 구현

--- src/profile/ProfileEdit.js
+++ src/profile/ProfileEdit.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions, } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, ActivityIndicator } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { MD3LightTheme as DefaultTheme, } from 'react-native-paper';
 import { Image } from 'expo-image';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Fontisto from 'react-native-vector-icons/Fontisto';
-
+import { auth } from '../../FireBase/DB';
+import { updateProfile } from 'firebase/auth';
+import * as ImagePicker from 'expo-image-picker';
+import { storage } from '../../FireBase/DB';
+import { ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const WINDOW_HEIGHT = Dimensions.get('window').height;
 
 const PROFILE_IMAGE_SIZE = WINDOW_HEIGHT * 0.18;
 
-const ProfileEdit = () => {
+const ProfileEdit = ({navigation}) => {
   const [newDisplayName, setNewDisplayName] = useState('');
-  const [newProfileImg, setNewProfileImg] = useState('');
+  const [selectImageUrl, setSelectImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
 
+  const dispatch = useDispatch();
   const uid = useSelector((state) => state.UID); // Current logged-in user's UID
   const displayName = useSelector((state) => state.displayName); // Current logged-in user's displayName
   const profileImg = useSelector((state) => state.profileImg); // Current logged-in user's profileImg
@@ -28,32 +34,96 @@ const ProfileEdit = () => {
     },
   };
 
+  const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
+
+  const uploadImage = async () => {
+    // 권한 확인 코드: 권한 없으면 물어보고, 승인하지 않으면 함수 종료
+    if (!status?.granted) {
+      const permission = await requestPermission();
+      if (!permission.granted) {
+        return null;
+      }
+    }
+    // 이미지 업로드 기능
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      quality: 1,
+      aspect: [1, 1]
+    });
+    if (result.cancelled) {
+      return null; // 이미지 업로드 취소한 경우
+    }
+    console.log(result);
+    // 이미지 업로드 결과 및 이미지 경로 업데이트
+    
+    setSelectImageUrl(result.assets[0].uri);
+  };
+
+  const uploadImageToFirebase = async (imageUri) => {
+    // 이미지 파일 이름 (예: image_12345.jpg)
+    const fileName = `profile_image_${new Date().getTime()}.jpg`;
+    const storageRef = ref(storage, `profileImages/${fileName}`);
+  
+    try {
+      // 이미지를 Blob 형태로 변환
+      const response = await fetch(imageUri);
+      const blob = await response.blob();
+
+      // Blob을 Firebase Storage에 업로드
+      await uploadBytesResumable(storageRef, blob);
+
+      // 업로드된 이미지의 URL 가져오기
+      const url = await getDownloadURL(storageRef);
+
+      return url;
+    } catch (error) {
+      console.error("Error uploading image: ", error);
+      return null;
+    }
+  };
+
   const profileEdit = async() => {
     try{
-      
+      setLoading(true);
+      const firebaseImageUrl = await uploadImageToFirebase(selectImageUrl);
+      await updateProfile(auth.currentUser, {
+        displayName: newDisplayName,
+        photoURL: firebaseImageUrl
+      }).then(() => {
+        dispatch({type: 'SET_DISPLAYNAME', payload: newDisplayName});
+        dispatch({type: 'SET_PROFILE_IMG', payload: firebaseImageUrl});
+        navigation.navigate('Home',{
+          screen:'프로필'
+        })
+      })
     }
     catch(error) {
-
+      console.error(error);
     }
-    finally {
-
+    finally{
+      setLoading(false);
     }
   }
   return (
-<View style={styles.container}>
+    <View style={styles.container}>
+      {loading &&
+        <View style={styles.overlay}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      }
       <View style={{ width: '100%', alignItems: 'center' }}>
         <TouchableOpacity
           style={{ marginBottom: 20, position: 'relative' }}
-          // onPress={uploadImage}
+          onPress={uploadImage}
         >
           <Image
-            source={profileImg ? { uri: profileImg } : require('../../img/defaultProfile.png')}
+            source={selectImageUrl ? { uri: selectImageUrl } : require('../../img/defaultProfile.png')}
             style={styles.profileImage}
           />
           <View style={styles.cameraIcon}>
             <Fontisto name="camera" size={PROFILE_IMAGE_SIZE * 0.13} color="#FFF" />
           </View>
-
         </TouchableOpacity>
         <View style={styles.textInputBox}>
           <View style={{ flexDirection: 'row' }}>
@@ -76,7 +146,7 @@ const ProfileEdit = () => {
         {/* {error && <Text style={{ color: 'red', textAlign: 'center' }}>{error}</Text>} */}
         <TouchableOpacity
           style={styles.signupButton}
-          // onPress={handleSignup}
+          onPress={profileEdit}
         >
           <Text style={styles.signupText}>프로필 수정</Text>
         </TouchableOpacity>
@@ -142,5 +212,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: WINDOW_HEIGHT * 0.02,
   },
+  overlay: {
+    ...StyleSheet.absoluteFill,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 5,
+},
 });
 export default ProfileEdit;
