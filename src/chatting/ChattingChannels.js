8298b549cda3054bc7817f69bc371f3454098ed5
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-11-30 13:09:34 +0900

    채팅방 reload 기능 추가

--- src/chatting/ChattingChannels.js
+++ src/chatting/ChattingChannels.js
@@ -1,78 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
-import { collection, query, where, getDocs, getFirestore } from 'firebase/firestore';
-import { fireStoreDB } from '../../FireBase/DB';
-import { useSelector } from 'react-redux';
-import { useNavigation } from '@react-navigation/native';
-
+import React, { useState, useEffect, useCallback } from "react";
+import { View, Text, Image, FlatList, StyleSheet, TouchableOpacity, RefreshControl } from "react-native";
+import { collection, query, where, getDocs, getFirestore } from "firebase/firestore";
+import { fireStoreDB } from "../../FireBase/DB";
+import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 const ChattingChannels = () => {
-    const [chatRooms, setChatRooms] = useState([]);
-    const uid = useSelector((state) => state.UID);
-    const navigation = useNavigation();
-    useEffect(() => {
-        const fetchChatRooms = async () => {
-          const chatRoomsRef = collection(fireStoreDB, "channels");
-          const q = query(chatRoomsRef, where(`participants.${uid}.uid`, "==", uid));
-      
-          try {
-            const querySnapshot = await getDocs(q);
-            const fetchedChatRooms = querySnapshot.docs.map(doc => {
-              const data = doc.data();
-              const participants = data.participants || {};
-              const otherUserId = Object.keys(participants).find(participantUid => participantUid !== uid);
-              const otherUser = participants[otherUserId] || {};
-              return {
-                id: doc.id,
-                otherUserId: otherUserId,
-                otherUserDisplayName: otherUser.displayName,
-                otherUserProfileImage: otherUser.profileImage,
-                ...data
-              };
-            });
-      
-            setChatRooms(fetchedChatRooms);
-          } catch (error) {
-            console.error("Error fetching chat rooms: ", error);
-          }
+  const [chatRooms, setChatRooms] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const uid = useSelector((state) => state.UID);
+  const navigation = useNavigation();
+
+  const fetchChatRooms = async () => {
+    const chatRoomsRef = collection(fireStoreDB, "channels");
+    const q = query(chatRoomsRef, where(`participants.${uid}.uid`, "==", uid));
+    try {
+      const querySnapshot = await getDocs(q);
+      const fetchedChatRooms = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        const participants = data.participants || {};
+        const otherUserId = Object.keys(participants).find((participantUid) => participantUid !== uid);
+        const otherUser = participants[otherUserId] || {};
+        return {
+          id: doc.id,
+          otherUserId: otherUserId,
+          otherUserDisplayName: otherUser.displayName,
+          otherUserProfileImage: otherUser.profileImage,
+          ...data,
         };
-        fetchChatRooms();
-      }, [uid]);
+      });
 
-    const goToChat = (chatRoom) => {
-        navigation.navigate('Chatting', { chatRoomId: chatRoom.id, chatRoom });
-    };
+      setChatRooms(fetchedChatRooms);
+    } catch (error) {
+      console.error("Error fetching chat rooms: ", error);
+    }
+  };
 
-    const renderItem = ({ item }) => (
-        <TouchableOpacity style={styles.chatRoomContainer} onPress={() => goToChat(item)}>
-          <Image source={{ uri: item.otherUserProfileImage }} style={styles.profileImage} />
-          <Text style={styles.displayName}>{item.otherUserDisplayName}</Text>
-        </TouchableOpacity>
-      );
+  useEffect(() => {
+    fetchChatRooms();
+  }, [uid]);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchChatRooms().then(() => setRefreshing(false));
+  }, []);
 
-    return (
-        <FlatList
-            data={chatRooms}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-        />
-    );
+  const goToChat = (chatRoom) => {
+    navigation.navigate("Chatting", { chatRoomId: chatRoom.id, chatRoom });
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.chatRoomContainer} onPress={() => goToChat(item)}>
+      <Image source={{ uri: item.otherUserProfileImage }} style={styles.profileImage} />
+      <Text style={styles.displayName}>{item.otherUserDisplayName}</Text>
+    </TouchableOpacity>
+  );
+  return <FlatList data={chatRooms} renderItem={renderItem} keyExtractor={(item) => item.id} refreshControl={
+    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+}/>;
 };
 
 const styles = StyleSheet.create({
-    chatRoomContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 10,
-    },
-    profileImage: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        marginRight: 10,
-    },
-    displayName: {
-        fontSize: 18,
-    },
+  chatRoomContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  displayName: {
+    fontSize: 18,
+  },
 });
 export default ChattingChannels;
